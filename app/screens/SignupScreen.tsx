febd08b068912b1dc6de03c6b1710ea125be74d2
dldnfla <lwr0221716@naver.com>
2023-10-21 20:24:35 +0900

    add backgroundImage

--- app/screens/SignupScreen.tsx
+++ app/screens/SignupScreen.tsx
@@ -1,12 +1,13 @@
-/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react-native/no-color-literals */
 import { TextField } from 'app/components';
 import { spacing } from 'app/theme';
-import { Button, Screen, Text} from "../components" // 여기 주의하세용.. 리액트에서 가져오는게 아니라 컴포넌트에 있는 버튼입니다.
-import { View,ViewStyle,StyleSheet,Image} from 'react-native';
+import { Button,Text} from "../components" // 여기 주의하세용.. 리액트에서 가져오는게 아니라 컴포넌트에 있는 버튼입니다.
+import { View,ViewStyle,StyleSheet,Image, ImageBackground} from 'react-native';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Profile = require("../../assets/images/app-basic-profile.png")
+const BackGround = require("../../assets/images/app-background.png")
 
 export const SignupScreen = (_props) => {
   const [userEmail, setUserEmail] = useState("")
@@ -49,24 +50,18 @@ export const SignupScreen = (_props) => {
   }
     
   return (
-      <Screen preset="auto"
-      contentContainerStyle={$screenContentContainer}
-      safeAreaEdges={["top", "bottom"]}
-    >
-
-      <Text preset = "bold" size = 'xxl'>
-        {"Stunning"}
-        {'\n'}
-        {'\n'}
-        {'\n'}
-      </Text>
-      <View>
-        <View style = {$profileimage}>
-          <Image source={Profile} style = {styles.image}/>
-        </View>
-      </View>
+      <ImageBackground source={BackGround} style = {styles.backgroundimage}>
+        <View style={$screenContentContainer}>
+         <Text preset = "bold" size = 'xxl' style = {styles.text}>
+          {"STUNNING"}
+        </Text>
         <View>
-          <Text >
+          <View style = {$profileimage}>
+            <Image source={Profile} style = {styles.image}/>
+          </View>
+        </View>
+        <View>
+          <Text>
             {'email'}
           </Text>
           <TextField 
@@ -77,8 +72,8 @@ export const SignupScreen = (_props) => {
             autoCapitalize="none" 
           />
           <Text>
-           {'password'}
-         </Text>
+            {'password'}
+           </Text>
          <TextField 
           value={userPassword}
           onChangeText={setUserPassword}
@@ -87,8 +82,7 @@ export const SignupScreen = (_props) => {
           autoCapitalize="none"
          />
         </View>
-        
-        <View style = {styles.fixToText}>
+        <View  style = {styles.fixToText} >
           <Button
             testID="Back-Button"
             text ="취소"
@@ -104,51 +98,59 @@ export const SignupScreen = (_props) => {
            onPress={registerUser}
           />
         </View>
-      </Screen>
+      </View> 
+      </ImageBackground>
     );
   };
+const styles = StyleSheet.create({
+    backgroundimage: {
+      backgroundColor:"#F6E4E4",
+      height: '100%',
+      width: '100%',
+    },
+    fixToText: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    image: {
+      height: 90,
+      width: 90,
+    },
+    text: {
+      marginTop: 50,
+    }
+  },
+);
   
 const $screenContentContainer: ViewStyle = {
-    paddingVertical: spacing.sm,
-    paddingHorizontal: spacing.xl,
-    // backgroundColor: '#FFFFFF'
+     paddingVertical: spacing.xl,
+     paddingHorizontal: spacing.xl,
+     justifyContent: "center",
+    
 }
-
 const $profileimage: ViewStyle = {
+  marginTop: '40%',
+  marginBottom: '10%',
   alignItems: 'center',
 }
 
 const $textField: ViewStyle = {
-    paddingVertical: spacing.xs,
-    marginTop: spacing.xxxs,
-    width: '100%',
-    display: "flex",
+  marginTop: 5,
+  marginBottom: 5,
 }
 
 const $tapButtonSignup: ViewStyle = {
-    marginTop: spacing.md,
-    borderRadius: 10,
-    backgroundColor: "#1DB43E",
-    width: "40%",
-    top: '300%',
+  borderRadius: 10,
+  backgroundColor: "#1DB43E",
+  width: "40%",
+  top: '410%',
 }
-
 const $tapButtonBack: ViewStyle = {
-  marginTop: spacing.md,
+  // marginTop: spacing.md,
   borderRadius: 10,
   backgroundColor: "#D63D3D",
   width: '40%',
-  top: '300%',
+  top: '410%',
 }
 
-const styles = StyleSheet.create({
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  image: {
-    height: 110,
-    width: 110,
-  },
-},);
 
