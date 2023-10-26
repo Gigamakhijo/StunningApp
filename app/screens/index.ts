3ad580249c5de9be0d90751ff0c8f62602972f7e
dldnfla <lwr0221716@naver.com>
2023-10-27 01:17:33 +0900

    add screen index

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -8,3 +8,5 @@ export * from "./DemoShowroomScreen/DemoShowroomScreen"
 export * from "./ErrorScreen/ErrorBoundary"
 // export other screens here
 export * from "./SignupScreen"
+export * from "./SetProfileScreen"
+export * from "./MainScreen"
