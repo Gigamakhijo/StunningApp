4bf2f3b9c42b4cab338869af5c090e6b83bfff5a
naonxj <cc4391@naver.com>
2023-11-04 16:00:10 +0900

    스크린추가, schedule text 구현실패작

--- app/screens/MainScreen.tsx
+++ app/screens/MainScreen.tsx
@@ -1,32 +1,58 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { Image, ImageBackground, View, ViewStyle } from "react-native"
-import { CalendarView } from "../components/CalendarView"
+// import React ,{useState, Component} from 'react';
+import { ImageBackground,Image, View, ViewStyle } from "react-native";
+import { Screen} from "../components";
+import { CalendarView} from "../components/CalendarView";
 export const MainScreen = (_props) => {
   const BackGround = require("../../assets/images/app-background.png")
   const WeeklyReport = require("../../assets/images/WeeklyChart_Line.png")
 
-  return (
-    <ImageBackground source={BackGround} style={$backgroundImage}>
-      <View style={$profileBox}>
-        <Image source={WeeklyReport} style={$weeklyreportImage} />
-        <CalendarView />
-      </View>
-    </ImageBackground>
+  return(
+    <Screen
+      preset="auto"
+      contentContainerStyle={$screenContentContainer}
+      safeAreaEdges={["top", "bottom"]}
+      backgroundColor="#FFFEFE"
+    >
+      <ImageBackground source={BackGround} style={$backgroundImage}>
+        <View style={$profileBox}>
+          <Image source={WeeklyReport} style={$weeklyreportImage}/>
+          <CalendarView />
+          {/* <Text style={$scheduletextstyle}>{new Date().toDateString()+" Schedule"}</Text> */}
+          {/* <View style={$scheduleBox}></View> */}
+        </View>
+      </ImageBackground>
+    </Screen>
   )
 }
-
+const $screenContentContainer: ViewStyle ={
+  // alignContent: 'flex-start',
+  flexDirection: 'column',
+}
 const $backgroundImage: ViewStyle = {
   backgroundColor: "#F6E4E4",
 }
 
 const $profileBox: ViewStyle = {
-  backgroundColor: "#FFFFFF",
+  backgroundColor: "FFFEFE",
   borderRadius: 53,
   height: "100%",
   marginTop: 77,
 }
+// const $scheduleBox: ViewStyle ={
+  
+//   backgroundColor: "white",
+//   borderRadius: 20,
+//   height: 100,
+//   marginTop: 10,
+//   marginLeft:20,
+//   marginRight:20,
+// }
 const $weeklyreportImage: ViewStyle = {
-  flexDirection: "row",
-  marginLeft: 335,
-  marginBottom: 10,
+  marginTop: 5,
+  marginLeft: 330,
 }
+// const $scheduletextstyle: ViewStyle ={
+//   marginLeft: 30,
+//   marginTop:5,
+// }
