35a91ba2253f17412cdff50c6780751f695d67ec
naonxj <cc4391@naver.com>
2023-11-08 23:32:09 +0900

    마이프로필 기본  스크린 생성

---
+++ app/screens/MyprofileScreen.tsx
@@ -0,0 +1,37 @@
+// import { observer } from "mobx-react-lite"
+import React, { }from "react"
+import { ImageBackground,View, ViewStyle} from "react-native";
+import { Screen} from "../components";
+// import { AppStackScreenProps } from "../navigators"
+
+export const MyprofileScreen = (_props) => {
+  const BackGround = require("../../assets/images/app-background.png")
+  return(
+    <Screen
+      preset="auto"
+      contentContainerStyle={$screenContentContainer}
+      safeAreaEdges={["top", "bottom"]}
+      backgroundColor="#FFFEFE"
+    >
+      <ImageBackground source={BackGround} style={$backgroundImage}>
+        <View style={$profileBox}>
+        </View>
+      </ImageBackground>
+    </Screen>
+  )
+}
+
+const $screenContentContainer: ViewStyle ={
+  // alignContent: 'flex-start',
+  flexDirection: 'column',
+}
+const $backgroundImage: ViewStyle = {
+  backgroundColor: "#F6E4E4",
+}
+
+const $profileBox: ViewStyle = {
+  backgroundColor: "FFFEFE",
+  borderRadius: 53,
+  height: "100%",
+  marginTop: 77,
+}
